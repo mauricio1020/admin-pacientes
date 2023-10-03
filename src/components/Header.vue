6857bb760826836a5eec996f52998e73a9ba6640
Forero Gonzalez <M2Forero@saludcapital.gov.co>
2023-10-03 17:28:11 -0500

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+
+     <h1 class="font-black text-5xl text-center">
+          Seguimiento Pacientes
+          <span class="text-cyan-400">Veterinaria</span>
+     </h1>
+
+
+</template>
